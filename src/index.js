394f29fcedcd0ab305300324dd86dd36a6894e4b
HelgaDp <106965471+HelgaDp@users.noreply.github.com>
2022-07-31 23:59:34 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,6 +12,37 @@ function convertToCelsium() {
 let temperatureCelsium = document.querySelector("#celsium");
 temperatureCelsium.addEventListener("click", convertToCelsium);
 
+
+function displayFahrenheitTemperature(event) {
+    event.preventDefault();
+    let temperatureElement = document.querySelector("#temperature");
+    
+    celsiusLink.classList.remove("active");
+    fahrenheitLink.classList.add("active");
+    
+    let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+    temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+
+function displayCelsiusTemperature(event) {
+    event.preventDefault();
+    let temperatureElement = document.querySelector("#temperature");
+    
+    celsiusLink.classList.add("active");
+    fahrenheitLink.classList.remove("active");
+    
+    temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+
+
+
 // Date
 function dateTime(currDate) {
   let days = [
